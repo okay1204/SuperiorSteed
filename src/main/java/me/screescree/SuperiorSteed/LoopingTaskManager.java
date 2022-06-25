b9c17c1cc07f33d7201a2c84a325204d2e4fbc49
okayyyy <zghan2005@gmail.com>
2022-06-25 15:32:16 -0700

    Add extrovert trait handler

--- src/main/java/me/screescree/SuperiorSteed/LoopingTaskManager.java
+++ src/main/java/me/screescree/SuperiorSteed/LoopingTaskManager.java
@@ -14,6 +14,7 @@ import me.screescree.SuperiorSteed.superiorhorse.features.comfortabilitystatmana
 import me.screescree.SuperiorSteed.superiorhorse.features.shavingsuse.ShavingsUseHorseTracker;
 import me.screescree.SuperiorSteed.superiorhorse.features.speedchanger.SendActionBarLoop;
 import me.screescree.SuperiorSteed.superiorhorse.features.traits.AngelHandler;
+import me.screescree.SuperiorSteed.superiorhorse.features.traits.ExtrovertHandler;
 import me.screescree.SuperiorSteed.superiorhorse.features.traits.FriendlyHandler;
 import me.screescree.SuperiorSteed.superiorhorse.features.traits.LonerHandler;
 
@@ -33,6 +34,7 @@ public class LoopingTaskManager {
         horseTasks.add(new LonerHandler());
         horseTasks.add(new AngelHandler());
         horseTasks.add(new FriendlyHandler());
+        horseTasks.add(new ExtrovertHandler());
     }
 
     public void start() {
