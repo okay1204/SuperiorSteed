cd6476231b32a1f834eabd92cca7dac6d4ffcf25
okayyyy <zghan2005@gmail.com>
2022-06-28 12:21:05 -0700

    Start HorseBreedGoal

---
+++ src/main/java/me/screescree/SuperiorSteed/SuperiorHorse/entity/goals/HorseBreedGoal.java
@@ -0,0 +1,10 @@
+package me.screescree.SuperiorSteed.superiorhorse.entity.goals;
+
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.animal.Animal;
+
+public class HorseBreedGoal extends BreedGoal {
+   public HorseBreedGoal(Animal var0, double var1) {
+      super(var0, var1, var0.getClass());
+   }
+}
